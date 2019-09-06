edb5d4c3c21ac145f83548f256d3a01bb03a1a01
Retro <mossiat.jeoffrey@outlook.com>
2019-09-06 09:40:57 +0200

    Push starter

---
+++ geekCulture/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DrawComponent } from './draw/draw.component';
+import { AppareilComponent } from './appareil/appareil.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DrawComponent,
+    AppareilComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ geekCulture/src/app/appareil/appareil.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.scss']
+})
+export class AppareilComponent implements OnInit {
+
+  appareilName: string = "Machine à laver";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
